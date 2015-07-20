0be56e4e2d3b668863f410426283f9ebb0f67aae
Lukas <Luke014@live.de>
2015-07-20 10:25:28 +0200

    creating BeaconManager instance

--- app/src/main/java/de/dhbwloerrach/beaconlocation/BeaconTools.java
+++ app/src/main/java/de/dhbwloerrach/beaconlocation/BeaconTools.java
@@ -1,11 +1,26 @@
 package de.dhbwloerrach.beaconlocation;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+
 import java.util.ArrayList;
 
 /**
  * Created by alirei on 20.07.2015.
  */
-public class BeaconTools {
+public class BeaconTools implements BeaconConsumer {
+    BeaconManager beaconManager;
+    Context context;
+
+    public BeaconTools(Context context){
+        this.context = context;
+        beaconManager = BeaconManager.getInstanceForApplication(this.context);
+        beaconManager.bind(this);
+    }
 
     public ArrayList GetBeacons(){
         return null;
@@ -17,5 +32,25 @@ public class BeaconTools {
 
     public Beacon GetBeacon(Integer major, Integer minor){
         return null;
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return this.context;
+    }
+
+    @Override
+    public void unbindService(ServiceConnection serviceConnection) {
+
+    }
+
+    @Override
+    public boolean bindService(Intent intent, ServiceConnection serviceConnection, int i) {
+        return false;
     }
 }
