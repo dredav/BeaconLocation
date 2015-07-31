29091692b0d13b58c8d71d2dce8f563bd01031f8
Lukas <Luke014@live.de>
2015-07-31 13:48:48 +0200

    created action menu for machines

--- app/src/main/java/de/dhbwloerrach/beaconlocation/activities/MachinesFragment.java
+++ app/src/main/java/de/dhbwloerrach/beaconlocation/activities/MachinesFragment.java
@@ -17,6 +17,7 @@ import de.dhbwloerrach.beaconlocation.R;
 import de.dhbwloerrach.beaconlocation.adapters.MachineAdapter;
 import de.dhbwloerrach.beaconlocation.database.DatabaseHandler;
 import de.dhbwloerrach.beaconlocation.models.Beacon;
+import de.dhbwloerrach.beaconlocation.models.FilterTyp;
 
 /**
  * Created by Lukas on 31.07.2015.
@@ -43,6 +44,12 @@ public class MachinesFragment extends Fragment implements IFragment {
         }
 
         ListView listView = (ListView) activity.findViewById(R.id.listView2);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO : go to view #5
+            }
+        });
         listView.setAdapter(adapter);
     }
 
@@ -53,12 +60,26 @@ public class MachinesFragment extends Fragment implements IFragment {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return false;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getActivity().getMenuInflater().inflate(R.menu.menu_machines, menu);
+        //return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return false;
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add_machine) {
+            // TODO : go to view #6
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
