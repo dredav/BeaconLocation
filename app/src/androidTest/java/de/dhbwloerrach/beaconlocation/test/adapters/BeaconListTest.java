624db919a03ded2fc9fab0737762649c0ed6e81b
David Dreher <dd@wesolix.com>
2015-07-31 10:51:20 +0200

    add BeaconListTest structur

---
+++ app/src/androidTest/java/de/dhbwloerrach/beaconlocation/test/adapters/BeaconListTest.java
@@ -0,0 +1,27 @@
+package de.dhbwloerrach.beaconlocation.test.adapters;
+
+import android.test.AndroidTestCase;
+
+import de.dhbwloerrach.beaconlocation.test.helpers.TestHelper;
+
+/**
+ * Created by David on 7/24/15.
+ */
+public class BeaconListTest extends AndroidTestCase {
+    private TestHelper helper = new TestHelper();
+
+    public BeaconListTest() {
+        super();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
