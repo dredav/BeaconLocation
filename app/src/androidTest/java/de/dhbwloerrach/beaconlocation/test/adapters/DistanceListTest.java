080a404f1d7facd6fd9255396e0caeb3a2bce019
David Dreher <dd@wesolix.com>
2015-07-31 10:38:01 +0200

    test travis build

--- app/src/androidTest/java/de/dhbwloerrach/beaconlocation/test/adapters/DistanceListTest.java
+++ app/src/androidTest/java/de/dhbwloerrach/beaconlocation/test/adapters/DistanceListTest.java
@@ -38,7 +38,7 @@ public class DistanceListTest extends AndroidTestCase {
     }
 
     public void testGetLast() throws Exception {
-        assertTrue(true);
+        assertTrue(false);
     }
 
     public void testAverageDistance() throws Exception {
