5a0b410b22fc77f7aad2a4a73508eeb43b31c466
Alisha Reiß <reisshea@dhbw-loerrach.de>
2015-08-04 12:12:13 +0200

    Noch nicht funktionierendes Etwas

--- app/src/main/java/de/dhbwloerrach/beaconlocation/activities/AddNewMachineFragment.java
+++ app/src/main/java/de/dhbwloerrach/beaconlocation/activities/AddNewMachineFragment.java
@@ -6,11 +6,14 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 import de.dhbwloerrach.beaconlocation.R;
+import de.dhbwloerrach.beaconlocation.database.DatabaseHandler;
 import de.dhbwloerrach.beaconlocation.models.Beacon;
+import de.dhbwloerrach.beaconlocation.models.Machine;
 
 /**
  * Created by alirei on 31.07.2015.
@@ -28,10 +31,40 @@ public class AddNewMachineFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
         commons = activity.getCommons();
         selectedBeacons = getArguments().getParcelableArrayList("selectedBeacons");
-        final Button button = (Button) activity.findViewById(R.id.button_cancel);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button cancelButton = (Button) activity.findViewById(R.id.button_cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 commons.switchFragment(ActivityCommons.FragmentType.BEACON_SEARCH);
+
+            }
+        });
+        final Button CommitButton = (Button) activity.findViewById(R.id.button_commit);
+        CommitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Textfeld auslesen
+                final EditText textField = (EditText) activity.findViewById(R.id.editText);
+                // Maschine erstellen
+                Machine newMachine = new Machine();
+                newMachine.setName(textField.getText().toString());
+                // Maschine in DB eintragen
+                DatabaseHandler databaseHandler = new DatabaseHandler(activity);
+                databaseHandler.createMachine(newMachine);
+                // Maschinen ID auslesen
+                Integer machineID;
+
+                // Beacons prüfen, ob sie bereits in der DB vorliegen und einrtagen
+                for (Beacon beacon: selectedBeacons){
+                    Beacon databaseBeacon = databaseHandler.getBeacon(beacon.getMinor(), beacon.getMajor(), beacon.getUuid());
+                    beacon.setMachineId(machineID);
+                    if (databaseBeacon != null){
+                        databaseHandler.createBeacon(beacon);
+                    }
+                    else{
+                        // WARNUNG fehlt
+                        databaseHandler.updateBeacon(beacon);
+                    }
+                }
+
 
             }
         });
