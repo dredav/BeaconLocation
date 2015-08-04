609d563b02cff542f67b2dc4b3ceda65850d49d7
Lukas <Luke014@live.de>
2015-08-04 12:14:53 +0200

    dynamically add minor text fields for machine (not yet working)

--- app/src/main/java/de/dhbwloerrach/beaconlocation/activities/AddManualMachineFragment.java
+++ app/src/main/java/de/dhbwloerrach/beaconlocation/activities/AddManualMachineFragment.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import de.dhbwloerrach.beaconlocation.R;
 import de.dhbwloerrach.beaconlocation.adapters.MachineAdapter;
@@ -22,6 +25,8 @@ import de.dhbwloerrach.beaconlocation.models.Machine;
  * Created by Lukas on 04.08.2015.
  */
 public class AddManualMachineFragment extends BaseFragment {
+    ArrayList<TextView> minors = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,35 +50,39 @@ public class AddManualMachineFragment extends BaseFragment {
     @Override
     protected boolean handleMenuClick(int itemId) {
         switch (itemId) {
+            case R.id.add_beacon:
+                TextView textView = new TextView(activity);
+                minors.add(textView);
+                final LinearLayout layout = (LinearLayout)activity.findViewById(R.id.minors);
+                layout.addView(textView);
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        layout.invalidate();
+                    }
+                });
+                return true;
             case R.id.save_machine:
                 // save machine to database
                 DatabaseHandler databaseHandler = new DatabaseHandler(activity);
 
-                TextView editText = (TextView)getActivity().findViewById(R.id.name);
+                TextView editText = (TextView)getActivity().findViewById(R.id.editText);
                 Machine machine = new Machine().setName(editText.getText().toString());
                 databaseHandler.createMachine(machine);
 
-                TextView minor1 = (TextView)getActivity().findViewById(R.id.minor1);
-                int minorId1 = Integer.parseInt(minor1.getText().toString());
-                // TODO : get beacons with all major and uuid
-                Beacon beacon1 = databaseHandler.getBeacon(minorId1, 0, 0);
-                if(beacon1 != null) {
-                    beacon1.setMachineId(machine.getId());
-                    databaseHandler.updateBeacon(beacon1);
-                } else {
-                    beacon1 = new Beacon().setMinor(minorId1).setMachineId(machine.getId());
-                    databaseHandler.createBeacon(beacon1);
-                }
-
-                TextView minor2 = (TextView)getActivity().findViewById(R.id.minor2);
-                int minorId2 = Integer.parseInt(minor2.getText().toString());
-                Beacon beacon2 = databaseHandler.getBeacon(minorId2, 0, 0);
-                if(beacon2 != null) {
-                    beacon2.setMachineId(machine.getId());
-                    databaseHandler.updateBeacon(beacon2);
-                } else {
-                    beacon2 = new Beacon().setMinor(minorId1).setMachineId(machine.getId());
-                    databaseHandler.createBeacon(beacon2);
+                for(TextView minor : minors){
+                    int minorId = Integer.parseInt(minor.getText().toString());
+                    // TODO : get beacons with all major and uuid
+                    Beacon beacon = databaseHandler.getBeacon(minorId, 0, "");
+                    if(beacon != null) {
+                        beacon.setMachineId(machine.getId());
+                        databaseHandler.updateBeacon(beacon);
+                    } else {
+                        beacon = new Beacon()
+                                .setMinor(minorId)
+                                .setMachineId(machine.getId());
+                        databaseHandler.createBeacon(beacon);
+                    }
                 }
                 return true;
             default:
