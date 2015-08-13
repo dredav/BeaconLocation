57f6cba4cd764dba0c4a9030c9f7870e82ebf89e
David Dreher <dd@wesolix.com>
2015-08-13 19:28:14 +0200

    Add delete beacon option to machine view; add dialog for deleting machine; Merge branch 'master' of http://github.com/dredav/BeaconLocation

--- app/src/main/java/de/dhbwloerrach/beaconlocation/activities/MachineFragment.java
+++ app/src/main/java/de/dhbwloerrach/beaconlocation/activities/MachineFragment.java
@@ -1,8 +1,9 @@
 package de.dhbwloerrach.beaconlocation.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -30,7 +31,6 @@ public class MachineFragment extends BaseFragment implements IBeaconListView {
     protected Boolean updatePaused = false;
     protected ArrayList<Beacon> selectedBeacons = new ArrayList<>();
     protected Menu menu;
-    protected ArrayList<Beacon> machineBeacons;
     protected Machine machine;
 
     @Nullable
@@ -51,108 +51,177 @@ public class MachineFragment extends BaseFragment implements IBeaconListView {
         activity.getCommons().startMonitoring(this);
         machine = getArguments().getParcelable("machine");
 
-        DatabaseHandler databaseHandler = new DatabaseHandler(activity);
-        machineBeacons = databaseHandler.getAllBeaconsByMachine(machine.getId());
-
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                adapter.clearItems();
-                adapter.addItems(machineBeacons);
-                adapter.notifyDataSetChanged();
-            }
-        });
+        updateBeaconListView();
 
         TextView textView = (TextView) activity.findViewById(R.id.name);
         textView.setText(machine.getName());
 
         ListView listView = (ListView) activity.findViewById(R.id.beaconList);
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                updatePaused=true;
+                updatePaused = true;
                 ListView listView = (ListView) parent;
                 Beacon beacon = (Beacon) listView.getAdapter().getItem(position);
-                if (selectedBeacons.contains(beacon)){
+
+                if (selectedBeacons.contains(beacon)) {
                     selectedBeacons.remove(beacon);
                     view.setBackgroundColor(Color.TRANSPARENT);
-                    if (selectedBeacons.isEmpty()){
-                        updatePaused=false;
+                    if (selectedBeacons.isEmpty()) {
+                        updatePaused = false;
                     }
-                }
-                else{
+                } else {
                     selectedBeacons.add(beacon);
                     view.setBackgroundColor(0xFF8db6cd);
                 }
 
-                //menu.getItem(0).setEnabled(!updatePaused);
-                //menu.getItem(1).setEnabled(updatePaused);
+                updateMenuButtons();
             }
         });
-
 
         listView.setAdapter(adapter);
     }
 
+    protected void updateMenuButtons() {
+        menu.findItem(R.id.add_beacon).setVisible(selectedBeacons.size() == 0);
+        menu.findItem(R.id.delete_machine).setVisible(selectedBeacons.size() == 0);
+        menu.findItem(R.id.delete_beacon).setVisible(selectedBeacons.size() != 0);
+    }
+
     @Override
     protected void createActionBarMenu(Menu menu) {
+        this.menu = menu;
+
         activity.getMenuInflater().inflate(R.menu.menu_machine, menu);
     }
 
     @Override
     protected boolean handleMenuClick(int itemId) {
-        switch (itemId){
-            case R.id.add_beacon:
-                Bundle bundle = new Bundle();
-                bundle.putParcelable("oldMachine", machine);
-                activity.getCommons().switchFragment(ActivityCommons.FragmentType.BEACON_SEARCH, bundle);
-                return true;
-            case R.id.delete_machine:
-                DatabaseHandler databaseHandler = new DatabaseHandler(activity);
-                for (Beacon beacon : machineBeacons) {
-                    databaseHandler.deleteBeacon(beacon);
-                }
-                databaseHandler.deleteMachine(machine);
-                activity.getCommons().switchFragment(ActivityCommons.FragmentType.BEACON_SEARCH);
-                return true;
+        DatabaseHandler databaseHandler = new DatabaseHandler(activity);
 
+        try {
+            switch (itemId){
+                case R.id.add_beacon:
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelable("oldMachine", machine);
+                    activity.getCommons().switchFragment(ActivityCommons.FragmentType.BEACON_SEARCH, bundle);
+                    return true;
+
+                case R.id.delete_machine:
+                    menuActionDeleteMachine(databaseHandler);
+                    return true;
+
+                case R.id.delete_beacon:
+                    menuActionDeleteBeacon(databaseHandler);
+                    return true;
+            }
+
+            return false;
+        } finally {
+            databaseHandler.close();
         }
-        return false;
     }
 
     @Override
     public void RefreshList(ArrayList<Beacon> beacons) {
-        if (!updatePaused) {
-            BeaconList visibleBeacons = new BeaconList();
-            visibleBeacons.addAll(beacons);
-            BeaconList filteredBeacons = visibleBeacons.filterByLast(5);
+        if(updatePaused) {
+            return;
+        }
 
-            final BeaconList beaconList = new BeaconList();
+        BeaconList visibleBeacons = new BeaconList();
+        visibleBeacons.addAll(beacons);
+        BeaconList filteredBeacons = visibleBeacons.filterByLast(5);
 
+        final BeaconList beaconList = new BeaconList();
+        DatabaseHandler databaseHandler = new DatabaseHandler(activity);
+        ArrayList<Beacon> machineBeacons = databaseHandler.getAllBeaconsByMachine(machine.getId());
+
+        try {
             //add visible Beacons, which are part of the machine
-            for(Beacon beacon : filteredBeacons) {
-                if(machineBeacons.contains(beacon)){
+            for (Beacon beacon : filteredBeacons) {
+                if (machineBeacons.contains(beacon)) {
                     beaconList.add(beacon);
                 }
             }
 
             //add Beacons, which are part of the machine and not visible
-            for(Beacon beacon : machineBeacons){
-                if(!beaconList.contains(beacon)){
+            for (Beacon beacon : machineBeacons) {
+                if (!beaconList.contains(beacon)) {
                     beaconList.add(beacon);
                 }
             }
-
-            activity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    adapter.clearItems();
-                    adapter.addItems(beaconList);
-                    adapter.notifyDataSetChanged();
-                }
-            });
+        } finally {
+            databaseHandler.close();
         }
+
+        updateBeaconListView();
+    }
+
+    protected void updateBeaconListView() {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                DatabaseHandler databaseHandler = new DatabaseHandler(activity);
+                try {
+                    adapter.clearItems();
+                    adapter.addItems(databaseHandler.getAllBeaconsByMachine(machine.getId()));
+                    adapter.notifyDataSetChanged();
+                } finally {
+                    databaseHandler.close();
+                }
+            }
+        });
+    }
+
+    protected void menuActionDeleteBeacon(final DatabaseHandler databaseHandler) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(R.string.alert_title_deleteBeacon);
+        builder.setMessage(String.format(activity.getResources().getString(R.string.alert_message_deleteBeacon), selectedBeacons.toString()));
+
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //
+            }
+        });
+
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                for (Beacon beacon : selectedBeacons) {
+                    selectedBeacons.remove(beacon);
+                    databaseHandler.deleteBeacon(beacon);
+                }
+                updateBeaconListView();
+                updateMenuButtons();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    protected void menuActionDeleteMachine(final DatabaseHandler databaseHandler) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(R.string.alert_title_deleteMachine);
+        builder.setMessage(R.string.alert_message_deleteMachine);
+
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //
+            }
+        });
+
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                for (Beacon beacon : databaseHandler.getAllBeaconsByMachine(machine.getId())) {
+                    databaseHandler.deleteBeacon(beacon);
+                }
+                databaseHandler.deleteMachine(machine);
+                activity.getCommons().switchFragment(ActivityCommons.FragmentType.MACHINES_VIEW);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     @Override
