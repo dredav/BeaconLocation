ea98a784dc0f16d991fd33172e2ca231abbdd130
David Dreher <dd@wesolix.com>
2015-09-10 10:23:54 +0200

    add BeaconListTest

--- app/src/androidTest/java/de/dhbwloerrach/beaconlocation/test/modles/BeaconListTest.java
+++ app/src/androidTest/java/de/dhbwloerrach/beaconlocation/test/modles/BeaconListTest.java
@@ -2,6 +2,11 @@ package de.dhbwloerrach.beaconlocation.test.modles;
 
 import android.test.AndroidTestCase;
 
+import java.util.List;
+
+import de.dhbwloerrach.beaconlocation.models.Beacon;
+import de.dhbwloerrach.beaconlocation.models.BeaconList;
+import de.dhbwloerrach.beaconlocation.models.FilterTyp;
 import de.dhbwloerrach.beaconlocation.test.helpers.TestHelper;
 
 /**
@@ -10,6 +15,10 @@ import de.dhbwloerrach.beaconlocation.test.helpers.TestHelper;
 public class BeaconListTest extends AndroidTestCase {
     private TestHelper helper = new TestHelper();
 
+    private BeaconList beacons;
+
+    private static final int COUNT_BEACONS = 10;
+
     public BeaconListTest() {
         super();
     }
@@ -17,8 +26,39 @@ public class BeaconListTest extends AndroidTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+
+        List<Beacon> beacons = helper.createBeacons(COUNT_BEACONS);
+        for(Beacon beacon : beacons) {
+            beacon.setRssi(helper.createRandom(-150, 0));
+        }
+
+        this.beacons = new BeaconList();
+        this.beacons.addAll(beacons);
     }
 
+    public void testSortByRSSI() {
+        beacons.Sort(FilterTyp.RSSI);
+
+        int lastRssi = 0;
+        for (int index = 0; index < beacons.size(); index ++) {
+            int currentRssi = beacons.get(index).getRssi();
+
+            assertTrue(currentRssi <= lastRssi);
+            lastRssi = currentRssi;
+        }
+    }
+
+    public void testSortByMinor() {
+        beacons.Sort(FilterTyp.Minor);
+
+        int lastMinor = 0;
+        for (int index = 0; index < beacons.size(); index ++) {
+            int currentMinor = beacons.get(index).getMinor();
+
+            assertTrue(currentMinor >= lastMinor);
+            lastMinor = currentMinor;
+        }
+    }
 
     @Override
     protected void tearDown() throws Exception {
